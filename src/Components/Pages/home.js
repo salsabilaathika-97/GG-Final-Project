6a507bb77dd1f05ed4a252d8813bc2c55bc114be
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-08-19 20:52:56 +0800

    add vercel redirect

--- src/Components/Pages/home.js
+++ src/Components/Pages/home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
     const handleClick = async () => {
         let client_id = '2bba7390150c42c9bad5b2fe1fe7a3d7';
         let scopes = `playlist-modify-private playlist-modify-public`;
-        let redirect_url = `http://localhost:3000/redirect`;
+        let redirect_url = `https://gg-final-project.vercel.app/redirect`;
         let spotifyUrl = `https:/accounts.spotify.com/authorize?client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_url)}&response_type=token&show_dialog=true`;
         window.location = spotifyUrl;
     }
